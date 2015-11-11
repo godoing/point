fd1a6d363ce94af9b9f9168b492dcd53a13914e0
godoing <20141105063T@mac04.local>
2015-11-11 21:24:40 +0800

    Initial Commit

---
+++ Uz1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Uz1
+//
+//  Created by 20141105063T on 15/11/11.
+//  Copyright © 2015年 20141105063T. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
